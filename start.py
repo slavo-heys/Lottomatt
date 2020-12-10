738516831d80147d40b03f66aa23b3e1314f87d4
Slavo Heys <47301102+slavo-heys@users.noreply.github.com>
2020-12-10 19:19:56 +0100

    Dopisany przycisk exit

--- start.py
+++ start.py
@@ -32,22 +32,41 @@ class Window(QMainWindow):
         button.setIcon(QtGui.QIcon("lotto_1.png"))
         button.setIconSize(QtCore.QSize(50,50))
         button.setToolTip("<h3>Dodaj wyniki nowego losowania</h3>")
+        button.clicked.connect(self.Add_wyniki)
 
-        button_1 = QPushButton("       Dodaj moje liczby", self)
+        button_1 = QPushButton("         Dodaj moje liczby", self)
         button_1.setGeometry(QRect(10,55,160,40))
         button_1.setIcon(QtGui.QIcon("lottery.png"))
         button_1.setIconSize(QtCore.QSize(50,50))
         button_1.setToolTip("<h3>Dodaj swój zestaw wytypowanych liczb</h3>")
+        button_1.clicked.connect(self.Add_liczby)
 
         button_2 = QPushButton("  Sprawdź powtórzenia", self)
         button_2.setGeometry(QRect(10,100,160,40))
         button_2.setIcon(QtGui.QIcon("lotto_2.png"))
         button_2.setIconSize(QtCore.QSize(40,40))
         button_2.setToolTip("<h3>Sprawdź powtórzenia liczb we wszystkich losowaniach.</h3>")
+        button_2.clicked.connect(self.Powtorzenia)
+
+        button_3 = QPushButton("     Wyjście z programu", self)
+        button_3.setGeometry(QRect(10,145,160,40))
+        button_3.setIcon(QtGui.QIcon("lotto_3.png"))
+        button_3.setIconSize(QtCore.QSize(40,40))
+        button_3.setToolTip("<h3>Bezpieczne wyjście z programu.</h3>")
+        button_3.clicked.connect(self.Exit)
 
 
+    def Add_wyniki(self):
+        return
 
+    def Add_liczby(self):
+        return
 
+    def Powtorzenia(self):
+        return
+
+    def Exit(self):
+        sys.exit()
 
 
 App = QApplication(sys.argv)
