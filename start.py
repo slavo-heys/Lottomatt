3779cf9b2c834856bd020b83e44db63935e5ca22
Slavo Heys <47301102+slavo-heys@users.noreply.github.com>
2020-12-12 15:30:21 +0100

    dodawanie losowan do bazy

--- start.py
+++ start.py
@@ -5,6 +5,8 @@ import sqlite3
 from time import strftime
 import random
 from PIL import ImageTk, Image
+import time
+from tkinter.messagebox import showinfo
 
 # Tworzenie bazy , jeśli nie istnieje
 conn = sqlite3.connect('baza.db')
@@ -12,29 +14,29 @@ c = conn.cursor()
 c.execute(
     """CREATE TABLE IF NOT EXISTS lotto(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        dzien int NOT NULL,
-        miesiac int NOT NULL,
-        rok int NOT NULL,
-        nr_losowania int NOT NULL,
-        l1 int NOT NULL,
-        l2 int NOT NULL,
-        l3 int NOT NULL,
-        l4 int NOT NULL,
-        l5 int NOT NULL,
-        l6 int NOT NULL);"""
+        dzien text NOT NULL,
+        miesiac text NOT NULL,
+        rok text NOT NULL,
+        nr_losowania text NOT NULL,
+        l1 text NOT NULL,
+        l2 text NOT NULL,
+        l3 text NOT NULL,
+        l4 text NOT NULL,
+        l5 text NOT NULL,
+        l6 text NOT NULL);"""
 )
 c.execute(
     """CREATE TABLE IF NOT EXISTS liczbyUser(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        dzien int NOT NULL,
-        miesiac int NOT NULL,
-        rok int NOT NULL,
-        lu1 int NOT NULL,
-        lu2 int NOT NULL,
-        lu3 int NOT NULL,
-        lu4 int NOT NULL,
-        lu5 int NOT NULL,
-        lu6 int NOT NULL
+        dzien text NOT NULL,
+        miesiac text NOT NULL,
+        rok text NOT NULL,
+        lu1 text NOT NULL,
+        lu2 text NOT NULL,
+        lu3 text NOT NULL,
+        lu4 text NOT NULL,
+        lu5 text NOT NULL,
+        lu6 text NOT NULL
     );"""
 )
 conn.commit()
@@ -91,8 +93,8 @@ class Program:
         but4.place(x=10, y=540)
 
     def dodaj_losowanie(self):
-        self.ramka1 = Frame(root,  height=596, width=650, bg="#FAFAD2")
-        self.ramka1.pack(padx=5, pady=5, side=RIGHT)
+        self.ramka1 = Frame(root,  height=200, width=650, bg="#FAFAD2")
+        self.ramka1.pack(padx=5, pady=5, side=TOP)
 
         # Formularz - dodaj liczby z losowania
         l1 = tk.Label(self.ramka1, text="Wstaw datę losowania:",
@@ -121,52 +123,58 @@ class Program:
 
         self.dzien = StringVar()
         self.e1 = tk.Entry(self.ramka1, textvariable=self.dzien,
-                      font=("Arial", 13), width=3)
+                           font=("Arial", 13), width=3)
         self.e1.place(x=229, y=10)
 
         self.miesiac = StringVar()
         self.e1a = tk.Entry(self.ramka1, textvariable=self.miesiac,
-                       font=("Arial", 13), width=3)
+                            font=("Arial", 13), width=3)
         self.e1a.place(x=316, y=10)
 
         self.rok = StringVar()
         self.e1b = tk.Entry(self.ramka1, textvariable=self.rok,
-                       font=("Arial", 13), width=5)
+                            font=("Arial", 13), width=5)
         self.e1b.place(x=407, y=10)
 
         self.nLosowania = StringVar()
         self.e2 = tk.Entry(self.ramka1, textvariable=self.nLosowania,
-                      font=("Arial", 13), width=8)
+                           font=("Arial", 13), width=8)
         self.e2.place(x=200, y=35)
+
+        self.liczbaJeden = StringVar()
+        self.liczbaDwa = StringVar()
+        self.liczbaTrzy = StringVar()
+        self.liczbaCztery = StringVar()
+        self.liczbaPiec = StringVar()
+        self.liczbaSzesc = StringVar()
         # liczba1
-        self.licz1 = StringVar()
-        self.e3 = tk.Entry(self.ramka1, textvariable=self.licz1,
-                      font=("Arial", 13), width=2)
+        self.e3 = tk.Entry(self.ramka1, textvariable=self.liczbaJeden,
+                           font=("Arial", 13), width=2)
         self.e3.place(x=200, y=70)
         # liczba2
-        self.licz2 = StringVar()
-        self.e4 = tk.Entry(self.ramka1, textvariable=self.licz2,
-                      font=("Arial", 13), width=2)
+
+        self.e4 = tk.Entry(self.ramka1, textvariable=self.liczbaDwa,
+                           font=("Arial", 13), width=2)
         self.e4.place(x=230, y=70)
         # liczba 3
-        self.licz3 = StringVar()
-        self.e5 = tk.Entry(self.ramka1, textvariable=self.licz3,
-                      font=("Arial", 13), width=2)
+
+        self.e5 = tk.Entry(self.ramka1, textvariable=self.liczbaTrzy,
+                           font=("Arial", 13), width=2)
         self.e5.place(x=260, y=70)
         # liczba 4
-        self.licz4 = StringVar()
-        self.e6 = tk.Entry(self.ramka1, textvariable=self.licz4,
-                      font=("Arial", 13), width=2)
+
+        self.e6 = tk.Entry(self.ramka1, textvariable=self.liczbaCztery,
+                           font=("Arial", 13), width=2)
         self.e6.place(x=290, y=70)
         # liczba 5
-        self.licz5 = StringVar()
-        self.e7 = tk.Entry(self.ramka1, textvariable=self.licz5,
-                      font=("Arial", 13), width=2)
+
+        self.e7 = tk.Entry(self.ramka1, textvariable=self.liczbaPiec,
+                           font=("Arial", 13), width=2)
         self.e7.place(x=320, y=70)
         # liczba 6
-        self.licz6 = StringVar()
-        self.e8 = tk.Entry(self.ramka1, textvariable=self.licz6,
-                      font=("Arial", 13), width=2)
+
+        self.e8 = tk.Entry(self.ramka1, textvariable=self.liczbaSzesc,
+                           font=("Arial", 13), width=2)
         self.e8.place(x=350, y=70)
         # przycisk zapisz do tabli i wyczyść dane
         przycisk1 = tk.Button(self.ramka1, text="Zapisz do bazy",
@@ -177,21 +185,114 @@ class Program:
                               bg="#90EE90", command=self.clear_dane)
         przycisk2.place(x=210, y=100)
 
+        przycisk3 = tk.Button(self.ramka1, text=" Zamknij to okno",
+                              fg="white", bg="blue", command=self.zamknij_okno)
+        przycisk3.place(x=260, y=160)
+
+    def zamknij_okno(self):
+        self.ramka1.destroy()
+
+    def info_liczba(self):
+        showinfo("Uwaga!", "Jedna z podanych liczb\n nie mieści się w zakresie 1-49")
+
+    def info_dzien(self):
+        showinfo("Uwaga!", "Liczba dni nie mieści się w zakresie 1-31")
+
+    def info_miesiac(self):
+        showinfo("Uwaga!", "Liczba miesiecy nie mieści się w zakresie 1-12")
+
+    def info_rok(self):
+        showinfo("Uwaga!", "Liczba rok nie mieści się w okreslonym zakresie")
+
+    def info_tesame(self):
+        showinfo("Uwaga!", "Jedna z liczba jest taka sama\njak pozostałe")
+
     def zapisz_do_bazy(self):
-        return
+        self.dzienLos = self.dzien.get()
+        self.miesiacLos = self.miesiac.get()
+        self.rokLos = self.rok.get()
+        self.numerLos = self.nLosowania.get()
+        self.liczbaPi = self.liczbaJeden.get()
+        self.liczbaD = self.liczbaDwa.get()
+        self.liczbaT = self.liczbaTrzy.get()
+        self.liczbaCz = self.liczbaCztery.get()
+        self.liczbaP = self.liczbaPiec.get()
+        self.liczbaSz = self.liczbaSzesc.get()
+
+        biezaceRok = strftime("%Y")
+        biezaceRok = int(biezaceRok)
+
+        dzienLos = int(self.dzienLos)
+        miesiacLos = int(self.miesiacLos)
+        rokLos = int(self.rokLos)
+        liczbaPi = int(self.liczbaPi)
+        liczbaD = int(self.liczbaD)
+        liczbaT = int(self.liczbaT)
+        liczbaCz = int(self.liczbaCz)
+        liczbaP = int(self.liczbaP)
+        liczbaSz = int(self.liczbaSz)
+
+        if dzienLos > 31 or dzienLos < 1:
+            self.info_dzien()
+        elif miesiacLos > 12 or miesiacLos < 1:
+            self.info_miesiac
+        elif rokLos > biezaceRok or rokLos < 1957:
+            self.info_rok()
+        elif liczbaPi > 49 or liczbaPi < 1:
+            self.info_liczba()
+        elif liczbaD > 49 or liczbaD < 1:
+            self.info_liczba()
+        elif liczbaT > 49 or liczbaT < 1:
+            self.info_liczba()
+        elif liczbaCz > 49 or liczbaCz < 1:
+            self.info_liczba()
+        elif liczbaP > 49 or liczbaP < 1:
+            self.info_liczba()
+        elif liczbaSz > 49 or liczbaSz < 1:
+            self.info_liczba()
+
+        elif liczbaPi == liczbaD or liczbaPi == liczbaT or liczbaPi == liczbaCz or liczbaPi == liczbaP or liczbaPi == liczbaSz:
+            self.info_tesame()
+        elif liczbaD == liczbaT or liczbaD == liczbaCz or liczbaD == liczbaP or liczbaD == liczbaSz:
+            self.info_tesame()
+        elif liczbaT == liczbaCz or liczbaT == liczbaP or liczbaT == liczbaSz:
+            self.info_tesame()
+        elif liczbaCz == liczbaP or liczbaCz == liczbaSz:
+            self.info_tesame()
+        elif liczbaP == liczbaSz:
+            self.info_tesame()
+        else:
+            conn = sqlite3.connect('baza.db')
+            c = conn.cursor()
+            c.execute("INSERT INTO lotto VALUES(NULL, :dzien, :miesiac, :rok, :nr_losowania, :l1, :l2, :l3, :l4, :l5, :l6)",
+                      {
+                          'dzien': self.dzienLos,
+                          'miesiac': self.miesiacLos,
+                          'rok': self.rokLos,
+                          'nr_losowania': self.numerLos,
+                          'l1': self.liczbaP,
+                          'l2': self.liczbaD,
+                          'l3': self.liczbaT,
+                          'l4': self.liczbaCz,
+                          'l5': self.liczbaP,
+                          'l6': self.liczbaSz
+                      })
+            conn.commit()
+            conn.close()
+            self.zamknij_okno()
+            print("zapisano")
 
     def clear_dane(self):
-        self.e1.delete(0,END)
-        self.e1a.delete(0,END)
-        self.e1b.delete(0,END)
-        self.e2.delete(0,END)
-        self.e3.delete(0,END)
-        self.e4.delete(0,END)
-        self.e5.delete(0,END)
-        self.e6.delete(0,END)
-        self.e7.delete(0,END)
-        self.e8.delete(0,END)
-
+        self.e1.delete(0, END)
+        self.e1a.delete(0, END)
+        self.e1b.delete(0, END)
+        self.e2.delete(0, END)
+        self.e3.delete(0, END)
+        self.e4.delete(0, END)
+        self.e5.delete(0, END)
+        self.e6.delete(0, END)
+        self.e7.delete(0, END)
+        self.e8.delete(0, END)
 
     def dodaj_liczby(self):
         self.ramka1 = Frame(root,  height=596, width=650, bg="#FAFAD2")
